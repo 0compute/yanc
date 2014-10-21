cb6b5c7b8d0e8f45246165ab8ecc956e3fe479c9
Arthur Noel <arthur@0compute.net>
2014-10-21 17:20:37 +0100

    supporting cpython 2.6+ only

--- setup.py
+++ setup.py
@@ -66,4 +66,21 @@ setup(
     entry_points = {
         "nose.plugins": ("yanc=yanc.yancplugin:YancPlugin",),
     },
+    classifiers = (
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Testing",
+    ),
 )
